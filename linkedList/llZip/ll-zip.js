26e2973d01cea4e64fdeeac600d2819dbec56d3a
Arthur Lozano <chillout885@gmail.com>
2021-03-16 00:07:54 -0500

    CC8

--- linkedList/llZip/ll-zip.js
+++ linkedList/llZip/ll-zip.js
@@ -1,7 +1,29 @@
 'use strict';
 
-function zipLists(list1, list2){
+const Node = require('../lib/node');
+const ll = require('../lib/linked-list');
 
+
+function zipLists(list1, list2) {
+  let dummynode = new Node();
+  let tail = dummynode;
+
+  while (!this.head) {
+    if (list1 === null) {
+      tail.next = list2;
+      break;
+    }
+    if (list2 === null) {
+      tail.next = list1;
+      break;
+    }
+    if (list1.value < list2.value) {
+      tail.next = list1;
+      list1 = list1.next;
+    }
+    tail = tail.next;
+  }
+  return dummynode.next;
 }
 
 module.exports = zipLists;
