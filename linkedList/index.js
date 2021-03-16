d0f07442c0d25436cca7d00f29c28f9ee6c0138b
Arthur Lozano <chillout885@gmail.com>
2021-03-15 21:48:25 -0500

    updated to the nth

--- linkedList/index.js
+++ linkedList/index.js
@@ -4,12 +4,21 @@ let LL = require('./lib/linked-list.js');
 
 let ll = new LL();
 
-ll.append('first');
+
+ll.append(1);
 
 ll.append(2);
 
-ll.append('three');
+ll.append(3);
 
 ll.append(4);
 
+ll.fromEnd(2);
 
+console.log(ll.fromEnd(1));
+// ll.insertFront();
+// ll.insertBefore();
+// ll.insertAfter(2, 5);
+// ll.insertFront();
+// ll.include();
+// ll.delete();
