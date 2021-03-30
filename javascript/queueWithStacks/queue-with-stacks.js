ff1c5c2aa74e6408ac9ddaa71d37879d1faed770
Arthur Lozano <chillout885@gmail.com>
2021-03-30 10:24:03 -0700

    CC 11

---
+++ javascript/queueWithStacks/queue-with-stacks.js
@@ -0,0 +1,38 @@
+'use strict';
+
+let Stack = require('./stack');
+
+class PseudoQueue {
+  constructor() {
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+  }
+  enqueue(value) {
+    while (this.stack1.storage.length > 0) {
+      let x = this.stack1.pop();
+      this.stack2.push(x)
+    }
+    this.stack1.push(value)
+    while (this.stack2.storage.length > 0) {
+      let x = this.stack2.pop()
+      this.stack1.push(x)
+    }
+    return this
+  }
+  dequeue() {
+    if (!this.stack1) {
+      return null
+    }
+    return this.stack1.pop()
+  }
+}
+
+module.exports = PseudoQueue;
+let newQueue = new PseudoQueue();
+
+newQueue.enqueue(1)
+newQueue.enqueue(2)
+newQueue.enqueue(3)
+newQueue.enqueue(5)
+newQueue.dequeue()
+console.log(newQueue.stack1.storage)
