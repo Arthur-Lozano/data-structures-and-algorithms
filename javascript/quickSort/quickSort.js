e3342a0474cba86a4c400c1643b3ec48df64f429
Arthur Lozano <chillout885@gmail.com>
2021-05-24 21:37:29 -0700

    initial

---
+++ javascript/quickSort/quickSort.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+const quickSort = values => {
+  if (values.length <= 1) {
+    return values;
+  }
+
+  let lessThanPivot = [];
+  let greaterThanPivot = [];
+  const pivot = values.shift();
+
+  for (let i = 0; i < values.length; i++) {
+    const value = values[i];
+    value <= pivot ? lessThanPivot.push(value) : greaterThanPivot.push(value);
+  }
+
+  return [...quickSort(lessThanPivot), pivot, ...quickSort(greaterThanPivot)];
+};
+
+module.exports = quickSort;
