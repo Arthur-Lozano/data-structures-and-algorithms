56366434a5520fb73297cadf99072176ae36d821
Arthur Lozano <chillout885@gmail.com>
2021-06-08 11:50:21 -0500

    update to testing

--- javascript/leftJoin/__tests__/left-join.test.js
+++ javascript/leftJoin/__tests__/left-join.test.js
@@ -1,9 +1,12 @@
 
 let hashMerge = require('../left-join');
 
-var mapLeft = new Map([['fond', 'enamored'], ['wrath', 'anger'], ['diligent', 'employed'], ['outift', 'garb'], ['guide', 'usher']]);
-var mapRight = new Map([['fond', 'averse'], ['wrath', 'delight'], ['diligent', 'idle'], ['guide', 'follow'], ['flow', 'jam']]);
+describe('Merges two Hashmaps', () => {
+  const mapLeft = new Map([['fond', 'enamored'], ['wrath', 'anger'], ['diligent', 'employed'], ['outift', 'garb'], ['guide', 'usher']]);
+  const mapRight = new Map([['fond', 'averse'], ['wrath', 'delight'], ['diligent', 'idle'], ['guide', 'follow'], ['flow', 'jam']]);
 
-test('Merges two Hashmaps', () => {
-  expect(merge(mapLeft, mapRight)
+  it('Add values to the hash tables', () => {
+    expect(expect(mapLeft.has('enamored')).toBeTruthy();
+    expect(expect(mapRight.has('averse')).toBeTruthy();
+  },
 });
