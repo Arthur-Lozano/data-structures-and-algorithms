223a88f4470ecb463cf9bac80cce2baa543f5382
Arthur Lozano <chillout885@gmail.com>
2021-06-08 01:45:59 -0500

    updated readme

--- javascript/leftJoin/__tests__/left-join.test.js
+++ javascript/leftJoin/__tests__/left-join.test.js
@@ -1,6 +1,9 @@
 
-let join = require('../left-join');
+let hashMerge = require('../left-join');
 
+var mapLeft = new Map([['fond', 'enamored'], ['wrath', 'anger'], ['diligent', 'employed'], ['outift', 'garb'], ['guide', 'usher']]);
+var mapRight = new Map([['fond', 'averse'], ['wrath', 'delight'], ['diligent', 'idle'], ['guide', 'follow'], ['flow', 'jam']]);
 
-
-it('merges two hash tables')
+test('Merges two Hashmaps', () => {
+  expect(merge(mapLeft, mapRight)
+});
--- javascript/leftJoin/left-join.js
+++ javascript/leftJoin/left-join.js
@@ -5,18 +5,18 @@ var mapLeft = new Map([['key1', 'synonym'], ['key2', 'synonym'], ['key3', 'synon
 console.log(mapLeft);
 var mapRight = new Map([['key1', 'antonym'], ['key2', 'antonym']]);
 console.log(mapRight.get('key1'));
-mapLeft.forEach((value, key) => {
-  let newEntry = [];
-  newEntry.push(key);
-  newEntry.push(value);
+let hashMerge = mapLeft.forEach((value, key) => {
+  let newItem = [];
+  newItem.push(key);
+  newItem.push(value);
   if (mapRight.get(key)) {
-    newEntry.push(mapRight.get(key));
+    newItem.push(mapRight.get(key));
   } else {
-    newEntry.push('NULL');
+    newItem.push('null');
   }
-  newMap.push(newEntry);
+  newMap.push(newItem);
 });
 console.log(newMap);
 
-
+module.exports = hashMerge;
 
