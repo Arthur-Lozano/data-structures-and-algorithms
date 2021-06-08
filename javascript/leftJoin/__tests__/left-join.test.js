4f3b04041e1b3f6ae79578ab1fdc2aa4bfcd34ba
Arthur Lozano <chillout885@gmail.com>
2021-06-08 12:03:36 -0500

    updated CC code

--- javascript/leftJoin/__tests__/left-join.test.js
+++ javascript/leftJoin/__tests__/left-join.test.js
@@ -3,10 +3,12 @@ let hashMerge = require('../left-join');
 
 describe('Merges two Hashmaps', () => {
   const mapLeft = new Map([['fond', 'enamored'], ['wrath', 'anger'], ['diligent', 'employed'], ['outift', 'garb'], ['guide', 'usher']]);
+
   const mapRight = new Map([['fond', 'averse'], ['wrath', 'delight'], ['diligent', 'idle'], ['guide', 'follow'], ['flow', 'jam']]);
 
+
   it('Add values to the hash tables', () => {
-    expect(expect(mapLeft.has('enamored')).toBeTruthy();
-    expect(expect(mapRight.has('averse')).toBeTruthy();
-  },
+    expect(expect(mapLeft.has('enamored')).toBeTruthy());
+    expect(expect(mapRight.has('averse')).toBeTruthy());
+  });
 });
--- javascript/leftJoin/left-join.js
+++ javascript/leftJoin/left-join.js
@@ -1,22 +1,21 @@
 'use strict';
 
-let newMap = [];
-var mapLeft = new Map([['key1', 'synonym'], ['key2', 'synonym'], ['key3', 'synonym']]);
-console.log(mapLeft);
-var mapRight = new Map([['key1', 'antonym'], ['key2', 'antonym']]);
-console.log(mapRight.get('key1'));
-let hashMerge = mapLeft.forEach((value, key) => {
-  let newItem = [];
-  newItem.push(key);
-  newItem.push(value);
-  if (mapRight.get(key)) {
-    newItem.push(mapRight.get(key));
-  } else {
-    newItem.push('null');
-  }
-  newMap.push(newItem);
-});
-console.log(newMap);
+let leftJoin = (mapLeft, mapRight) => {
+  let newMap = [];
+  mapLeft.forEach((value, key) => {
+    let newItem = [];
+    newItem.push(key);
+    newItem.push(value);
+    if (mapRight.get(key)) {
+      newItem.push(mapRight.get(key));
+    } else {
+      newItem.push('null');
+    }
+  });
+  return newMap;
+};
 
-module.exports = hashMerge;
+module.exports = leftJoin;
 
+// var mapLeft = new Map([['key1', 'synonym'], ['key2', 'synonym'], ['key3', 'synonym']]);
+// var mapRight = new Map([['key1', 'antonym'], ['key2', 'antonym']]);
