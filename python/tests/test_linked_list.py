7d0552bcf306fb94560e4b3c910f87f7c3d541ee
Arthur Lozano <chillout885@gmail.com>
2021-12-18 21:17:42 -0800

    linked list and test creation

--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,84 @@
-from linked_list.linked_list import LinkedList
+from linked_list.linked_list import Node, LinkedList
+import pytest
 
 
-def test_import():
-    assert LinkedList
+def test_node_instance():
+    node = Node(1, None)
+    assert node.next == None
+    assert node.value == 1
+
+
+def test_node_instance_fail():
+    node = Node(1, None)
+    assert node.next != node
+    assert node.value != 2
+
+
+def test_linked_list():
+    node = Node(1, None)
+    ll = LinkedList(node)
+    assert ll.head == node
+
+
+def test_linked_list_empty():
+    ll = LinkedList()
+    assert ll.head == None
+
+
+def test_insert_to_empty_linked_list():
+    # ll.head = apple
+    ll = LinkedList()
+    ll.insert("apple")
+    assert ll.head.value == "apple"
+
+
+def test_insert_to_linked_list():
+    # ll
+    # node1
+    # node2
+    # ll.head = apple
+    # apple.next = pear
+    # pear.next = None
+
+    # [apple] -> [pear] -> None
+
+    ll = LinkedList()
+    # head is none
+    node1 = Node("apple")
+    # ll.head is none
+    ll.head == node1
+    # ll.head is apple
+    node2 = Node("pear")
+    # apple.next is none
+    node1.next = node2
+    # apple.next is pear
+    # [apple] -> [pear] -> None
+    ll.insert("bananna")
+    # [bananna] -> [apple] -> [pear]
+    assert ll.head.value == "bananna"
+
+
+def test_multiple_insert_to_linked_list():
+    linked_list = LinkedList()
+    linked_list.insert("50")
+    linked_list.insert("10")
+    linked_list.insert("10")
+    linked_list.insert("30")
+    linked_list.insert("10")
+    actual = linked_list.head.next.value
+    expected = "30"
+    assert actual == expected
+
+
+def test_includes():
+    linked_list = LinkedList(Node("10", Node("20", Node("30", Node("40", Node("50"))))))
+    actual = linked_list.includes("30")
+    expected = True
+    assert actual == expected
+
+
+def test_str():
+    linked_list = LinkedList(Node("10", Node("20", Node("30", Node("40", Node("50"))))))
+    actual = linked_list.to_string()
+    expected = "{'10'}{'20'} {'30'}{'40'}{'50'} ->None"
+    assert actual == expected
