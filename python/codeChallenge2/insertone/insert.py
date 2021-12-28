c5df019d4d20526295db491f95a7a110f4f3ff17
Arthur Lozano <chillout885@gmail.com>
2021-12-27 22:13:02 -0800

    Linked List from the k-th

--- python/codeChallenge2/insertone/insert.py
+++ python/codeChallenge2/insertone/insert.py
@@ -10,14 +10,14 @@ def insertShitArray(list, val):
  midpoint = len(list)//2
  currentindex = len(list)-1
 
-  while currentindex >= midpoint:
-  if len(list) == currentindex -1
-  list.append[currentindex-1]
+   while currentindex >= midpoint:
+      if len(list) == currentindex -1
+          list.append[currentindex-1]
   else:
-    list[currentindex + 1] = list[currentindex]
-    currentindex -= 1
+      list[currentindex + 1] = list[currentindex]
+      currentindex -= 1
 
 list[currentindex +1] = val
-  return list
+    return list
 
 insertShitArray([2,4,6,-8], 5)
