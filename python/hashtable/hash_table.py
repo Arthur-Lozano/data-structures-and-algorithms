838691035e5f1e1dcc2f09a44663fc19023cb20a
Arthur <chillout885@gmail.com>
2022-03-12 15:40:56 -0800

    updated testing

--- python/hashtable/hash_table.py
+++ python/hashtable/hash_table.py
@@ -3,62 +3,42 @@ from link_list import LinkedList
 
 class HashTable:
     def __init__(self, size=1024):
-
         self.size = size
-        self.bucket = [None] * self.size
+        self.buckets = [None] * self.size
 
     def hash(self, key):
-
-        index = 0
-
+        hash_sum = 0
         for char in key:
-
-            index += ord(char)
-
-        index *= 599
-        index %= self.size
-
+            numeric_value = ord(char)
+            hash_sum += numeric_value
+        product = hash_sum * 599
+        index = product % self.size
         return index
 
     def add(self, key, value):
-
         index = self.hash(key)
-
-        if self.bucket[index] is None:
-            self.bucket[index] = LinkedList()
-
-        bucket = self.bucket[index]
+        if not self.buckets[index]:
+            self.buckets[index] = LinkedList()
+        bucket = self.buckets[index]
         bucket.insert([key, value])
 
     def get(self, key):
-
         index = self.hash(key)
-
-        if self.bucket[index] is None:
-            return None
-
-        current = self.bucket[index].head
-
-        while current is True:
+        if self.buckets[index] is None:
+            raise KeyError()
+        current = self.buckets[index].head
+        while current:
             if current.value[0] == key:
                 return current.value[1]
             current = current.next
 
-        return None
-
     def contains(self, key):
-
         index = self.hash(key)
-
-        if self.bucket[index] is None:
+        if self.buckets[index] is None:
             return False
-
-        current = self.bucket[index].head
-
-        while current is True:
+        current = self.buckets[index].head
+        while current:
             if current.value[0] == key:
                 return True
             current = current.next
-
-        else:
-            return False
+        return False
