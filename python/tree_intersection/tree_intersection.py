28b6d0ddb92ad7096b80da9a910c7b612afd5853
Arthur <chillout885@gmail.com>
2022-03-17 22:02:29 -0700

    cc

---
+++ python/tree_intersection/tree_intersection.py
@@ -0,0 +1,15 @@
+from hash_table import HashTable
+
+
+def tree_intersection(bt1, bt2):
+    matches = []
+    binary_tree1 = bt1.pre_order()
+    binary_tree2 = bt2.pre_order()
+    hashtable = HashTable()
+
+    for value in binary_tree1:
+        hashtable.add(key=str(value), value=value)
+    for value in binary_tree2:
+        if hashtable.contains(str(value)):
+            matches.append(value)
+    return matches
